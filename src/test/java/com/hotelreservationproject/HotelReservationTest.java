98e001947b2d069f9ce2a96b38e38e3f6794df34
anamfazal <anamfazal94@gmail.com>
2020-10-30 09:41:36 +0530

    Added: Welcome message

---
+++ src/test/java/com/hotelreservationproject/HotelReservationTest.java
@@ -0,0 +1,8 @@
+package com.hotelreservationproject;
+
+public class HotelReservationTest {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to hotel reservation project");
+    }
+}
